5e2f9639db949d3494866fd72bbfde3558c4f3a2
Tom Patterson <jemboo@gmail.com>
2025-08-26 03:43:29 -0400

    Still

--- GeneSort.Sorter/Sortable/SorterTestSet.fs
+++ GeneSort.Sorter/Sortable/SorterTestSet.fs
@@ -6,7 +6,7 @@ open FSharp.UMX
 open GeneSort.Core
 open GeneSort.Sorter
 
-[<Struct; CustomEquality; NoComparison>]
+
 type SorterTestSet =
     { Id: Guid<sorterTestSetId>
       sorterTests: SorterTest[] }
@@ -19,22 +19,10 @@ type SorterTestSet =
         let sortingWidth = SortableArray.sortingWidth arrays.[0].sortableArrays.[0]
         { Id = id; sorterTests = Array.copy arrays; }
 
-    override this.Equals(obj) =
-        match obj with
-        | :? SorterTestSet as other ->
-            this.Id = other.Id
-        | _ -> false
-
-    override this.GetHashCode() =
-        hash (this.Id, hash this.sorterTests)
 
     member this.SortableArrayType with get() = SortableArray.getSortableArrayType (this.sorterTests.[0]).sortableArrays.[0]
 
     member this.SortingWidth with get() = SortableArray.sortingWidth (this.sorterTests.[0]).sortableArrays.[0]
-
-    interface IEquatable<SorterTestSet> with
-        member this.Equals(other) =
-            this.Id = other.Id
 
 
 module SorterTestset = ()
