dc5474fa0f483b2c6e4dd20c4393d96c9dca9825
Tom Patterson <jemboo@gmail.com>
2026-02-01 13:26:31 -0500

    reorg

---
+++ GeneSort.Benchmark/CoreBench.fs
@@ -0,0 +1,123 @@
+﻿namespace global
+
+open BenchmarkDotNet.Attributes
+open System
+open GeneSort.Core
+open FSharp.UMX
+open GeneSort.Sorting
+open GeneSort.Sorting.Sortable
+open GeneSort.SortingOps
+open GeneSort.Sorting.Sorter
+open GeneSort.Model.Sorter.Ce
+open GeneSort.Model.Sorter
+open GeneSort.Sorting.Sortable.SortableIntArray
+
+
+//| Method           | size   | Mean           | Error        | StdDev       | Gen0     | Gen1     | Gen2     | Allocated |
+//|----------------- |------- |---------------:|-------------:|-------------:|---------:|---------:|---------:|----------:|
+//| createPermSafe   | 10     |       374.3 ns |      7.45 ns |     18.00 ns |   0.0286 |        - |        - |     480 B |
+//| createPermUnsafe | 10     |       310.3 ns |      7.52 ns |     21.70 ns |   0.0210 |        - |        - |     352 B |
+//| createPermSafe   | 100    |       985.1 ns |     27.57 ns |     81.28 ns |   0.0916 |        - |        - |    1560 B |
+//| createPermUnsafe | 100    |       508.9 ns |     15.50 ns |     45.44 ns |   0.0420 |        - |        - |     712 B |
+//| createPermSafe   | 1000   |     7,709.3 ns |    152.71 ns |    425.69 ns |   0.7324 |        - |        - |   12360 B |
+//| createPermUnsafe | 1000   |     2,111.6 ns |     40.30 ns |     33.66 ns |   0.2556 |   0.0038 |        - |    4312 B |
+//| createPermSafe   | 10000  |    83,858.5 ns |  1,668.36 ns |  1,638.55 ns |   7.0801 |   0.6104 |        - |  120362 B |
+//| createPermUnsafe | 10000  |    18,781.2 ns |    373.80 ns |    654.67 ns |   2.3804 |        - |        - |   40313 B |
+//| createPermSafe   | 100000 | 1,084,067.6 ns | 21,191.29 ns | 41,331.99 ns | 330.0781 | 330.0781 | 330.0781 | 1200535 B |
+//| createPermUnsafe | 100000 |   175,756.3 ns |  1,150.11 ns |  1,019.54 ns | 110.8398 | 110.8398 | 110.8398 |  400374 B |
+
+[<MemoryDiagnoser>]
+type PermutationBench() =
+
+    [<Params(10, 100, 1000, 10000, 100000)>]
+    member val size = 0 with get, set
+
+    member val arrayA = [||] with get, set
+    member val arrayB = [||] with get, set
+    member val arrayC = [||] with get, set
+
+    [<GlobalSetup>]
+    member this.Setup() =
+        this.arrayA <- Array.init this.size (fun dex -> (dex + 2) % this.size)
+        this.arrayB <- Array.init this.size (fun dex -> (dex + 7) % this.size)
+        this.arrayC <- Array.zeroCreate this.size
+
+
+    [<Benchmark>]
+    member this.createPermSafe() =
+        let yab = permutation.create this.arrayA
+        yab.Order
+
+
+    [<Benchmark>]
+    member this.createPermUnsafe() =
+        let yab = permutation.createUnsafe this.arrayA
+        yab.Order
+
+
+
+
+
+//SortingWidth    Dimension       EdgeLength      level   LevelSet        LevelSetVV
+//12                3               4               6       19              5
+//24                3               8               12      61              13
+//36                3               12              18      127             25
+//48                3               16              24      217             41
+//72                3               24              36      469             85
+//96                3               32              48      817             145
+//16                4               4               8       85              8
+//32                4               8               16      489             33
+//48                4               12              24      1469            86
+//64                4               16              32      3281            177
+//96                4               24              48      10425           519
+//128               4               32              64      23969           1143
+//24                6               4               12      1751            18
+//48                6               8               24      32661           151
+//72                6               12              36      204763          676
+//96                6               16              48      782153          2137
+//144               6               24              72      5375005         11963
+//192               6               32              96      21533457        42955
+//32                8               4               16      38165           33
+//64                8               8               32      2306025         526
+//96                8               12              48      30162301        3788
+//128               8               16              64      197018321       17575
+
+
+
+
+
+[<MemoryDiagnoser>]
+type GetLevelSetBench() =
+
+    [<Params(2, 4, 8)>]
+    member val dim = 0 with get, set
+    
+    [<Params(2, 4)>]
+    member val edgeLength = 0 with get, set
+
+    // We can compute a dynamic lattice level in Setup 
+    // or just set a default. Usually level = (dim * edge) / 2 is the "densest" part.
+    member val latticeLevel = 0 with get, set
+
+    [<GlobalSetup>]
+    member this.Setup() =
+        // Calculating a mid-point level for the most intensive test
+        this.latticeLevel <- (this.dim * this.edgeLength) / 2
+
+    [<Benchmark(Baseline = true)>]
+    member this.getLevelSet() =
+        let d = UMX.tag<latticeDimension> this.dim
+        let e = UMX.tag<latticeDistance> this.edgeLength
+        let l = UMX.tag<latticeDistance> this.latticeLevel
+        
+        LatticePoint.getLevelSet d l e
+        |> Seq.iter ignore // Force evaluation of the sequence
+
+    [<Benchmark>]
+    member this.getLevelSetNew() =
+        let d = UMX.tag<latticeDimension> this.dim
+        let e = UMX.tag<latticeDistance> this.edgeLength
+        let l = UMX.tag<latticeDistance> this.latticeLevel
+        
+        LatticePoint.getLevelSet d l e
+        |> Seq.iter ignore // Force evaluation of the ResizeArray
