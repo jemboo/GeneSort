9e2b8b65dd256cf75654dc1589522c55d1b88224
Tom Patterson <jemboo@gmail.com>
2026-03-04 11:04:24 -0500

    Update MutationSegmentSetResults.fs

--- GeneSort.SortingOps/SortingResult/MutationSegmentSetResults.fs
+++ GeneSort.SortingOps/SortingResult/MutationSegmentSetResults.fs
@@ -10,47 +10,59 @@ type mutationSegmentSetResults =
     private
         { 
           sortingMutationSegments : sortingMutationSegment []
-          sortingResults: Dictionary<Guid<sortingId>, sortingResult>
+          sortingSegmentResults: Dictionary<Guid<sortingMutationSegmentId>, Dictionary<Guid<sortingId>, sortingResult>>
           evalMap: Dictionary<Guid<sorterId>, sortingMutationSetTag>
         }
     with
     static member create (sortingMutationSegments: sortingMutationSegment []) =
 
         let evalMap = Dictionary<Guid<sorterId>, sortingMutationSetTag>()
+        let sortingSegmentResults = Dictionary<Guid<sortingMutationSegmentId>, Dictionary<Guid<sortingId>, sortingResult>>()
+
         for segment in sortingMutationSegments do
+            let segmentResults = Dictionary<Guid<sortingId>, sortingResult>()
+            sortingSegmentResults.[segment.Id] <- segmentResults
+
             for (sorterId, sortingTag) in segment.MakeSorterIdsWithSortingTags do
                 let mutationSetTag = SortingMutationSetTag.create segment.Id sortingTag
                 evalMap.[sorterId] <- mutationSetTag
 
-        let sortingResults = Dictionary<Guid<sortingId>, sortingResult>()
-        for segment in sortingMutationSegments do
             match segment.SortingMutator with
             | sortingMutator.Single _ ->
                 for id in segment.GetSortingIds do
-                    sortingResults.[id] <- (singleSortingResult.create id |> sortingResult.Single)
+                    segmentResults.[id] <- (singleSortingResult.create id |> sortingResult.Single)
             | sortingMutator.Pair _ ->
                 for id in segment.GetSortingIds do
-                    sortingResults.[id] <- (splitPairsSortingResult.create id |> sortingResult.SplitPairs)
+                    segmentResults.[id] <- (splitPairsSortingResult.create id |> sortingResult.SplitPairs)
 
         {
             sortingMutationSegments = sortingMutationSegments
-            sortingResults = sortingResults
+            sortingSegmentResults = sortingSegmentResults
             evalMap = evalMap
         }
 
     member this.SortingMutationSegments with get() = this.sortingMutationSegments
-    member this.SortingResults with get() = this.sortingResults
+    member this.SortingSegmentResults with get() = this.sortingSegmentResults
     member this.EvalMap with get() = this.evalMap
 
     member this.UpdateSortingResults (newEval: sorterEval) =
         match this.evalMap.TryGetValue(newEval.SorterId) with
         | false, _ -> failwithf "SorterId %A not found in evalMap." newEval.SorterId
         | true, mutationSetTag ->
+            let segmentId = SortingMutationSetTag.getMutationSegmentId mutationSetTag
             let sortingId = SortingMutationSetTag.getSortingParentId mutationSetTag
             let modelTag  = SortingMutationSetTag.getModelTag mutationSetTag
-            match this.sortingResults.TryGetValue(sortingId) with
-            | false, _ -> failwithf "SortingId %A not found in sortingResults." sortingId
-            | true, sortingResult ->
-                SortingResult.UpdateSorterEval modelTag newEval sortingResult
+            match this.sortingSegmentResults.TryGetValue(segmentId) with
+            | false, _ -> failwithf "SegmentId %A not found in sortingSegmentResults." segmentId
+            | true, segmentResults ->
+                match segmentResults.TryGetValue(sortingId) with
+                | false, _ -> failwithf "SortingId %A not found in segment %A." sortingId segmentId
+                | true, sortingResult ->
+                    SortingResult.UpdateSorterEval modelTag newEval sortingResult
+
+    member this.GetSegmentResults (segmentId: Guid<sortingMutationSegmentId>) =
+        match this.sortingSegmentResults.TryGetValue(segmentId) with
+        | false, _ -> failwithf "SegmentId %A not found in sortingSegmentResults." segmentId
+        | true, segmentResults -> segmentResults
 
 module MutationSegmentSetResults = ()
