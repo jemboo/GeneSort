2a0a3fbf6ae25d7706c353ef97dfe19e921a5f59
Tom Patterson <jemboo@gmail.com>
2026-03-18 11:08:21 -0400

    before query params id change

---
+++ GeneSort.SortingResults/SortingSetFilter.fs
@@ -0,0 +1,24 @@
+﻿namespace GeneSort.SortingResults
+
+open FSharp.UMX
+open GeneSort.Sorting
+open GeneSort.SortingOps
+open System.Collections.Generic
+open System
+open GeneSort.Sorting.Sorter
+
+[<Struct; StructuralEquality; NoComparison>]
+type levelSetFilter =
+    private {
+        sorterCount:     int<sorterCount>
+        isSorted:    bool
+    }
+    static member create (sorterCount: int<sorterCount>) (isSorted: bool) =
+        { sorterCount = sorterCount; isSorted = isSorted }
+    member this.SorterCount with get() : int<sorterCount> = this.sorterCount
+    member this.IsSorted    with get() : bool             = this.isSorted
+
+
+module SortingSetFilter = ()
+
+
