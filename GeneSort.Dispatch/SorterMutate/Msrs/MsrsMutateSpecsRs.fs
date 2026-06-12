4441ae5d43666391fc0e35b8db25cb95caead4c9
Tom Patterson <jemboo@gmail.com>
2026-06-12 05:35:15 -0400

    scaffold Mutate models

---
+++ GeneSort.Dispatch/SorterMutate/Msrs/MsrsMutateSpecsRs.fs
@@ -0,0 +1,187 @@
+﻿namespace GeneSort.Dispatch.V1.SorterMutate.Msrs
+
+open FSharp.UMX
+open GeneSort.Model.Sorting.V1
+open GeneSort.Dispatch.V1
+open GeneSort.Core
+open GeneSort.Project.V1
+open GeneSort.SortingOps
+open GeneSort.Eval.V1
+open GeneSort.Sorting
+open GeneSort.Dispatch.V1.SorterMutate
+
+
+module MsrsMutateSpecsRs = 
+
+
+    let rngType = 
+            (runParameters.rngTypeKey, 
+            [CommonSorterMutate.projectRngType;] |> List.map RngType.toString)
+
+
+    let sorterEvalType = 
+            (runParameters.sorterEvalTypeKey, 
+            [ sorterEvalType.V2 ;] |> List.map SorterEvalType.toString)
+
+
+    let sorterEvalSelection = 
+            (runParameters.sorterEvalSelectionType, 
+            [ sorterEvalSelectionType.Tmb 6<sorterCount> ; ] |> List.map SorterEvalSelectionType.toString)
+
+    let sorterEvalMeasure = 
+            (runParameters.sorterEvalMeasureKey, 
+            [ sorterEvalMeasure.CeSt (1.0, true); ] |> List.map SorterEvalMeasure.toString)
+    
+    // SortingWidths
+    let smallSortingWidths = 
+            (runParameters.sortingWidthKey, [4;5;6;7;8;9;10;11;12] |> List.map string)
+    let mediumSortingWidths = 
+            (runParameters.sortingWidthKey, [14;16;18;20;22] |> List.map string)
+
+
+    // MutationRates
+    let mutationRates =
+            (runParameters.mutationRateKey, [1.0] |> List.map string)
+    let insertionRates =
+            (runParameters.insertionRateKey, [0.1;] |> List.map string)
+    let deletionRates =
+            (runParameters.deletionRateKey, [0.1;] |> List.map string)
+    let modificationRates =
+            (runParameters.modificationRateKey, [0.01; 0.05; 0.1] |> List.map string)
+
+
+    // SorterCounts
+    let testChildCount = (runParameters.sorterChildCountKey, ["10";] )
+    let smallChildCount = (runParameters.sorterChildCountKey, ["1";] )
+    let mediumChildCount = (runParameters.sorterChildCountKey, ["10";] )
+    let largeChildCount = (runParameters.sorterChildCountKey, ["100";] )
+
+    let testParentCount = (runParameters.sorterParentCountKey, ["100";] )
+    let smallParentCount = (runParameters.sorterParentCountKey, ["10";] )
+    let mediumParentCount = (runParameters.sorterParentCountKey, ["100";] )
+    let largeParentCount = (runParameters.sorterParentCountKey, ["1000";] )
+
+
+
+    // SimpleSorterModelTypes
+    let allSimpleSorterModelTypes = 
+            (runParameters.simpleSorterModelTypeKey, 
+             [simpleSorterModelType.Msce] |> List.map SimpleSorterModelType.toString)
+
+
+
+    let standardEnhancer (host: IRunHost) (rp: runParameters) : runParameters =
+        let qp = host.RunDb.MakeQueryParamsFromRunParams rp (outputDataType.Run host.Run.RunName)  
+        rp.WithDatabaseName(Some host.Run.DatabaseName)
+          .WithRunName(Some host.Run.RunName)
+          .WithRunFinished(Some false)
+          .WithId (Some qp.Value.Id)
+
+    
+    let private standardSorterModelTypeFilter (rp: runParameters) =
+        maybe {
+            let! smt = rp.GetSimpleSorterModelType()
+            let! sw = rp.GetSortingWidth()
+            let has2factor = (%sw % 2 = 0)
+            let isPowerOf2 = (%sw &&& (%sw - 1) = 0)
+            let isGt4 = (%sw > 4)
+            let validMsce = (smt = simpleSorterModelType.Msce)
+            let validMssi = (smt = simpleSorterModelType.Mssi) && has2factor
+            let validMsrs = (smt = simpleSorterModelType.Msrs) && has2factor
+            let validMsuf4 = (smt = simpleSorterModelType.Msuf4) && isPowerOf2 && isGt4
+            return! if validMsce || validMssi || validMsrs || validMsuf4 then Some rp else None
+        }
+
+
+    module Specs =
+
+        let Rand_Test (executorType: sorterMutateExecutorType)  : runHostSpec = {
+            DatabaseName = MsrsMutateDbs.RandomStandard.Uniform.dbName
+            RunName = sprintf @"Rand-Test_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelection
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                smallSortingWidths
+                allSimpleSorterModelTypes
+                testParentCount
+                testChildCount
+            ]
+            Filter = standardSorterModelTypeFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 2
+        }
+
+
+        let Rand_Small (executorType: sorterMutateExecutorType) : runHostSpec = {
+            DatabaseName = MsrsMutateDbs.RandomStandard.Uniform.dbName
+            RunName = sprintf @"Rand-Small_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelection
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                smallSortingWidths
+                allSimpleSorterModelTypes
+                testParentCount
+                largeChildCount
+            ]
+            Filter = standardSorterModelTypeFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 8
+        }
+
+        let Rand_Medium (executorType: sorterMutateExecutorType) : runHostSpec = {
+            DatabaseName = MsrsMutateDbs.RandomStandard.Uniform.dbName
+            RunName = sprintf @"Rand-Medium_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelection
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                mediumSortingWidths
+                allSimpleSorterModelTypes
+                largeParentCount
+                largeChildCount
+            ]
+            Filter = standardSorterModelTypeFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 4
+        }
+
+    type configType =
+        | Rand_Test
+        | Rand_Small
+        | Rand_Medium
+
+    let Configs = Map.ofList 
+                    [ 
+                        (configType.Rand_Test, Specs.Rand_Test); 
+                        (configType.Rand_Small, Specs.Rand_Small);
+                        (configType.Rand_Medium, Specs.Rand_Medium);
+                    ]
+
+    let getConfig (config: configType) (executorType: sorterMutateExecutorType) : runHostSpec =
+        let specFunc = Configs.[config]
+        specFunc executorType
+
+
---
+++ GeneSort.Dispatch/SorterMutate/Msuf4/Msuf4MutateSpecsRm.fs
@@ -0,0 +1,226 @@
+﻿namespace GeneSort.Dispatch.V1.SorterMutate.Msuf4
+
+
+open FSharp.UMX
+open GeneSort.Model.Sorting.V1
+open GeneSort.Dispatch.V1
+open GeneSort.Core
+open GeneSort.Project.V1
+open GeneSort.SortingOps
+open GeneSort.Dispatch.V1.SortableTest
+open GeneSort.Dispatch.V1.SorterEval
+open GeneSort.Sorting
+open GeneSort.Eval.V1
+open GeneSort.Dispatch.V1.SorterMutate
+
+
+module Msuf4MutateSpecsRm =
+
+    let rngType = 
+            (runParameters.rngTypeKey, 
+            [CommonSorterMutate.projectRngType;] |> List.map RngType.toString)
+
+
+    let sorterEvalType = 
+            (runParameters.sorterEvalTypeKey, 
+            [ sorterEvalType.V1; ] |> List.map SorterEvalType.toString)
+
+    //let sorterEvalSelectionType = 
+    //        (runParameters.sorterEvalSelectionType, 
+    //        [ sorterEvalSelectionType.TopN 10<sorterCount>;  
+    //          sorterEvalSelectionType.Tmb 6<sorterCount>;  
+    //          sorterEvalSelectionType.ValueSpan 10<sorterCount>; ] |> List.map SorterEvalSelectionType.toString)
+
+    let sorterEvalSelectionType = 
+            (runParameters.sorterEvalSelectionType, 
+            [ sorterEvalSelectionType.ValueSpan 5<sorterCount>;] |> List.map SorterEvalSelectionType.toString)
+
+
+    let sorterEvalMeasure = 
+            (runParameters.sorterEvalMeasureKey, 
+            [ sorterEvalMeasure.CeSt (1.0, true);] |> List.map SorterEvalMeasure.toString)
+    
+    // SortingWidths
+    let smallSortingWidths = SortableTestMergeSpecs.smallSortingWidths
+    let mediumSortingWidths = SortableTestMergeSpecs.mediumSortingWidths
+
+    // MergeDimensions
+    let allMergeDimensions = SortableTestMergeSpecs.allMergeDimensions
+    let lowMergeDimensions = SortableTestMergeSpecs.lowMergeDimensions
+    let highMergeDimensions = SortableTestMergeSpecs.highMergeDimensions
+
+    // DataFormats
+    let onlyDataFormat = 
+            (runParameters.sortableDataFormatKey, 
+            [CommonSorterEval.mergeSortableDataFormat] |> List.map SortableDataFormat.toString)
+    
+    // MergeSuffixTypes
+    let bothMergeSuffixTypes = SortableTestMergeSpecs.bothMergeSuffixTypes
+    let vv1SuffixType = SortableTestMergeSpecs.vv1SuffixType
+
+
+
+    // MutationRates
+    let mutationRates =
+            (runParameters.mutationRateKey, [1.0] |> List.map string)
+    let insertionRates =
+            (runParameters.insertionRateKey, [0.1;] |> List.map string)
+    let deletionRates =
+            (runParameters.deletionRateKey, [0.1;] |> List.map string)
+    let modificationRates =
+            (runParameters.modificationRateKey, [0.0025; 0.005; 0.01; 0.015; 0.02; 0.03; 0.04; 0.05 ] |> List.map string)
+
+
+    // SorterCounts
+    let testChildCount = (runParameters.sorterChildCountKey, ["10";] )
+    let smallChildCount = (runParameters.sorterChildCountKey, ["10";] )
+    let mediumChildCount = (runParameters.sorterChildCountKey, ["100";] )
+    let largeChildCount = (runParameters.sorterChildCountKey, ["100000";] )
+
+    // SimpleSorterModelTypes
+    let msceModelType = 
+            (runParameters.simpleSorterModelTypeKey, 
+             [simpleSorterModelType.Msce] |> List.map SimpleSorterModelType.toString)
+    let allSimpleSorterModelTypes = SorterEvalSpecsRandom.allSimpleSorterModelTypes
+
+
+
+    let standardEnhancer (host: IRunHost) (rp: runParameters) : runParameters =
+        let qp = host.RunDb.MakeQueryParamsFromRunParams rp (outputDataType.Run host.Run.RunName)  
+        rp.WithDatabaseName(Some host.Run.DatabaseName)
+          .WithRunName(Some host.Run.RunName)
+          .WithRunFinished(Some false)
+          .WithId (Some qp.Value.Id)
+
+
+
+    let private paramMapFilter (rp: runParameters) : runParameters option = 
+        maybe {
+            let! smt = rp.GetSimpleSorterModelType()
+            let! sw = rp.GetSortingWidth()
+            let! md = rp.GetMergeDimension()
+            let! mst = rp.GetMergeSuffixType()
+        
+            let has2factor = (%sw % 2 = 0)
+            let isMuf4able = (MathUtils.isAPowerOfTwo %sw)
+            let isMuf6able = (%sw % 3 = 0) && (MathUtils.isAPowerOfTwo (%sw / 3))
+
+            // We bind to unit just to enforce the filter
+            let! _ = 
+                match smt with
+                | simpleSorterModelType.Msce -> Some ()
+                | simpleSorterModelType.Mssi | simpleSorterModelType.Msrs -> 
+                    if has2factor then Some () else None
+                | simpleSorterModelType.Msuf4 -> 
+                    if (isMuf4able && %sw < 256) then Some () else None
+                | simpleSorterModelType.Msuf6 -> 
+                    if isMuf6able then Some () else None
+                | _ -> None
+
+            // Merge dimension check: If it doesn't divide, return None to stop
+            if (%sw % %md <> 0) then return! None
+        
+            // Suffix check: If it's NoSuffix and width > 128, return None to stop
+            if (mst.IsNoSuffix && %sw > 128) then return! None
+        
+            return rp
+        }
+
+
+    module Specs =
+
+        let Rand_Test (executorType: sorterMutateExecutorType)  : runHostSpec = {
+            DatabaseName = Muf4MutateDbs.RandomMerge.Uniform.dbName
+            RunName = sprintf @"Rand-test_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelectionType
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                SortableTestMergeSpecs.testSortingWidths
+                allSimpleSorterModelTypes
+                SortableTestMergeSpecs.testMergeDimensions
+                SortableTestMergeSpecs.noSuffixSuffixType
+                onlyDataFormat
+                largeChildCount
+            ]
+            Filter = paramMapFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 4
+        }
+
+        let Rand_Small (executorType: sorterMutateExecutorType) : runHostSpec = {
+            DatabaseName = Muf4MutateDbs.RandomMerge.Uniform.dbName
+            RunName = sprintf @"Rand-Small_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelectionType
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                smallSortingWidths
+                msceModelType
+                lowMergeDimensions
+                bothMergeSuffixTypes
+                onlyDataFormat
+                largeChildCount
+            ]
+            Filter = paramMapFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 8
+        }
+
+        let Rand_Medium (executorType: sorterMutateExecutorType) : runHostSpec = {
+            DatabaseName = Muf4MutateDbs.RandomMerge.Uniform.dbName
+            RunName = sprintf @"Rand-Medium_%s" (SorterMutateExecutorType.toString executorType) |> UMX.tag
+            RunDescription = "Standard binning for Msce/Mssi/Msrs/Msuf4"
+            Spans = [
+                rngType
+                sorterEvalSelectionType
+                sorterEvalMeasure
+                sorterEvalType
+                mutationRates
+                insertionRates
+                deletionRates
+                modificationRates
+                mediumSortingWidths
+                msceModelType
+                lowMergeDimensions
+                bothMergeSuffixTypes
+                onlyDataFormat
+                largeChildCount
+            ]
+            Filter = paramMapFilter
+            Enhancer = standardEnhancer
+            AllowOverwrite = false |> UMX.tag
+            MaxParallel = 4
+        }
+
+    type configType =
+        | Rand_Test
+        | Rand_Small
+        | Rand_Medium
+
+    let Configs = Map.ofList 
+                    [ 
+                        (configType.Rand_Test, Specs.Rand_Test); 
+                        (configType.Rand_Small, Specs.Rand_Small);
+                        (configType.Rand_Medium, Specs.Rand_Medium);
+                    ]
+
+    let getConfig (config: configType) (executorType: sorterMutateExecutorType) : runHostSpec =
+        let specFunc = Configs.[config]
+        specFunc executorType
+
+
